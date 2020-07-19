e63079c1a0f7b454560d58421f9c77a1fa8c82bc
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 17:46:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-array.join(" , ")
+  if array.length > 2
+    last_word = array.pop
+    array[0..array.length-2].join(" , ") + " and " + last_word
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array
+  end
 end
